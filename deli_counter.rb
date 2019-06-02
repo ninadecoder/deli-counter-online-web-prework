8fbe49b1bff8846eeb97a0cbca16c98f5616304f
Nina Kazzim <kazzim@icloud.com>
2019-06-02 18:32:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def line(arr)
+  size = arr.size
+
+  if size < 1
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{arr}"
+  end
+end
+
+def take_a_number(arr, name)
+  size = arr.size
+  newPos = size + 1
+  
+  if size < 1 
+    arr.push "#{newPos}. #{name}"
+  else
+    arr.push "#{newPos}. #{name}"
+  end
+  puts "Welcome, #{name}. You are number #{newPos} in line."
+
+end
+
+def now_serving(arr)
+  arr.shift
+  
+  
+  
+  puts "#{arr}"
+end
